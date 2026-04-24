30d2a8909ba36198e064989ab8018240a975600e
Abdelkhaleq Mohamed <abdelkhaleqmohamed535@gmail.com>
2026-04-24 16:45:06 +0300

    initial spring boot project setup

---
+++ src/main/java/com/fix/fixnow/FixnowApplication.java
@@ -0,0 +1,13 @@
+package com.fix.fixnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fix/fixnow/FixnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fix.fixnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
